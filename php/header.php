6f4c5399926ef155a93cee9d498b23797360a3c1
João de Sousa <joao@joaodesousa.com>
2020-03-05 00:23:34 +0000

    Initial Commit

---
+++ php/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta name="title" content="João de Sousa">
+    <meta name="description" content="I manage projects, but I also like to learn and to code stuff.">
+
+    <title>João de Sousa</title>
+    
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" SameSite=None>
+    
+    <script src="https://kit.fontawesome.com/e68082d4e6.js" crossorigin="anonymous" SameSite=None></script>
+</head>
+<body>
